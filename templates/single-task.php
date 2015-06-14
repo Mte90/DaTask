0b80197ff5026763d88d915837b0057ff29743b1
Mte90 <mte90smanettator@gmail.com>
2015-06-14 23:05:43 +0200

    fix #16

--- templates/single-task.php
+++ templates/single-task.php
@@ -7,11 +7,16 @@ get_header();
 ?>
 
 <div id="content-main" class="main" role="main">
+    <div class="col-md-8">
 	<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php include(wo_get_template_part( 'content', 'single-task', false )); ?>
+		<?php
+		include(wo_get_template_part( 'content', 'single-task', false ));
+		?>
 
-	<?php endwhile; // end of the loop. ?>
+	<?php endwhile; // end of the loop.  ?>
+    </div>
+    <?php get_sidebar(); ?>
 </div>
 <?php
 get_footer();
