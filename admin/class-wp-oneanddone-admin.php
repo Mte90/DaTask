c70530c03eb9b2df6c084f506c9cfe268a4a7a5d
Mte90 <mte90smanettator@gmail.com>
2015-07-08 18:50:45 +0200

    working on settings

--- admin/class-wp-oneanddone-admin.php
+++ admin/class-wp-oneanddone-admin.php
@@ -56,9 +56,12 @@ class Wp_Oneanddone_Admin {
 
 		// Load admin JavaScript after jQuery loading
 		add_action( 'admin_print_footer_scripts', array( $this, 'enqueue_admin_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_files' ) );
 
 		// At Glance Dashboard widget for your cpts
 		add_filter( 'dashboard_glance_items', array( $this, 'cpt_dashboard_support' ), 10, 1 );
+		// Add the options page and menu item.
+		add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
 
 		// Add an action link pointing to the options page.
 		$plugin_basename = plugin_basename( plugin_dir_path( realpath( dirname( __FILE__ ) ) ) . $this->plugin_slug . '.php' );
@@ -112,6 +115,27 @@ class Wp_Oneanddone_Admin {
 		return self::$instance;
 	}
 
+	
+	/**
+	 * Register and enqueue admin-specific style sheet.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    null    Return early if no settings page is registered.
+	 */
+	public function enqueue_admin_files() {
+		if ( !isset( $this->plugin_screen_hook_suffix ) ) {
+			return;
+		}
+		$screen = get_current_screen();
+		if ( $this->plugin_screen_hook_suffix == $screen->id || strpos( $_SERVER[ 'REQUEST_URI' ], 'index.php' ) || strpos( $_SERVER[ 'REQUEST_URI' ], get_bloginfo( 'wpurl' ) . '/wp-admin/' ) ) {
+			wp_enqueue_style( $this->plugin_slug . '-admin-styles', plugins_url( 'assets/css/admin.css', __FILE__ ), array( 'dashicons' ), Wp_Oneanddone::VERSION );
+		}
+		if ( $this->plugin_screen_hook_suffix === $screen->id ) {
+			wp_enqueue_script( $this->plugin_slug . '-admin-script', plugins_url( 'assets/js/admin.js', __FILE__ ), array( 'jquery', 'jquery-ui-tabs' ), Wp_Oneanddone::VERSION );
+		}
+	}
+	
 	/**
 	 * Register and enqueue admin-specific style sheet.
 	 * @return    null    Return early if no settings page is registered.
@@ -129,14 +153,6 @@ class Wp_Oneanddone_Admin {
 				});
 			});
 			</script>';
-			return;
-		}
-		if ( !isset( $this->plugin_screen_hook_suffix ) ) {
-			return;
-		}
-
-		if ( $this->plugin_screen_hook_suffix == $screen->id ) {
-			wp_enqueue_script( $this->plugin_slug . '-admin-script', plugins_url( 'assets/js/admin.js', __FILE__ ), array( 'jquery', 'jquery-ui-tabs' ), Wp_Oneanddone::VERSION );
 		}
 	}
 
@@ -146,27 +162,8 @@ class Wp_Oneanddone_Admin {
 	 * @since    1.0.0
 	 */
 	public function add_plugin_admin_menu() {
-
-		/*
-		 * Add a settings page for this plugin to the Settings menu.
-		 *
-		 * NOTE:  Alternative menu locations are available via WordPress administration menu functions.
-		 *
-		 *        Administration Menus: http://codex.wordpress.org/Administration_Menus
-		 *
-		 * @TODO:
-		 *
-		 * - Change 'Page Title' to the title of your plugin admin page
-		 * - Change '$this->plugin_name' to the text for menu item for the plugin settings page
-		 * - Change 'manage_options' to the capability you see fit
-		 *   For reference: http://codex.wordpress.org/Roles_and_Capabilities
-		 */
-		$this->plugin_screen_hook_suffix = add_options_page(
-			__( 'Page Title', $this->plugin_slug ), $this->plugin_name, 'manage_options', $this->plugin_slug, array( $this, 'display_plugin_admin_page' )
-		);
 		/*
 		 * Settings page in the menu
-		 * 
 		 */
 		$this->plugin_screen_hook_suffix = add_menu_page( __( 'Page Title', $this->plugin_slug ), $this->plugin_name, 'manage_options', $this->plugin_slug, array( $this, 'display_plugin_admin_page' ), 'dashicons-hammer', 90 );
 	}
