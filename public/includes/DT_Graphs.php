a5eed0b93663c26393270980e5267d183587b458
Daniele Scasciafratte <mte90net@gmail.com>
2016-09-20 19:01:48 +0200

    new internal code for graphs

--- public/includes/DT_Graphs.php
+++ public/includes/DT_Graphs.php
@@ -26,25 +26,35 @@ class DT_Graphs {
    * Generate a graph of the task created
    *
    * @since    1.0.0
-   * @param array $atts The attributes.
    */
   public function task_number() {
-    global $wpdb;
+    $args = array(
+	  'post_type' => 'task',
+	  'posts_per_page' => -1,
+	  'date_query' => array(
+		array(
+		    'after' => '12 month ago',
+		),
+	  ),
+    );
+    $query = new WP_Query( $args );
 
-    $postcountbymonth = $wpdb->get_results( 'select * from (select MONTH(post_date) as mo,YEAR(post_date) as ye,count(ID) as co from ' . $wpdb->posts . " where (post_status='publish' and post_type='task') group by MONTH(post_date),YEAR(post_date) order by post_date desc limit 12) a order by ye asc,mo asc" );
-
-    $postcounts = array();
-    foreach ( $postcountbymonth as $pc ) {
-	$month = $pc->mo;
-	if ( count( $month ) < 2 ) {
-	  $month = '0' . $month;
+    $postquery = array();
+    foreach ( $query->posts as $post ) {
+	$date = explode( ' ', $post->post_date );
+	if ( !isset( $postquery[ $date[ 0 ] ] ) ) {
+	  $postquery[ $date[ 0 ] ] = 0;
 	}
-	$postcounts[] = array( 'date' => "new Date('$pc->ye-$month-01T24:00:00Z')", 'value' => $pc->co );
+	$postquery[ $date[ 0 ] ] += 1;
+    }
+
+    foreach ( $postquery as $key => $count ) {
+	$postcounts[] = array( 'date' => "new Date('" . $key . "T24:00:00Z')", 'value' => $count );
     }
     echo '<div id="postcreated"></div>'
     . '<script>' .
     'MG.data_graphic({
-        title: "' . __( 'Show the task created in the last 12 months', DT_TEXTDOMAIN ) . '",
+        title: "' . sprintf( __( 'The %s tasks created in the last 12 months', DT_TEXTDOMAIN ), count( $query->posts ) ) . '",
         data: ' . str_replace( ')"', ')', str_replace( '"new', 'new', wp_json_encode( $postcounts, JSON_NUMERIC_CHECK ) ) ) . ',
         width: 800,
         height: 400,
@@ -65,24 +75,54 @@ class DT_Graphs {
    * Generate a graph of the task monthly activity
    *
    * @since    1.0.0
+   * @param	   array $atts The attribute.
    */
-  public function task_month_activity() {
-    global $wpdb;
+  public function task_month_activity( $atts ) {
+    extract( shortcode_atts( array(
+	  'list' => true,
+			  ), $atts ) );
+    $args = array(
+	  'post_type' => 'wdslp-wds-log',
+	  'posts_per_page' => -1,
+	  'tax_query' => array(
+		array(
+		    'taxonomy' => 'wds_log_type',
+		    'field' => 'slug',
+		    'terms' => array( 'datask' ),
+		),
+	  ),
+	  'date_query' => array(
+		array(
+		    'after' => '12 month ago',
+		),
+	  ),
+    );
+    $query = new WP_Query( $args );
 
-    $postcountbymonth = $wpdb->get_results( 'select * from (select MONTH(post_date) as mo,YEAR(post_date) as ye,count(ID) as co from ' . $wpdb->posts . " where (post_status='publish' and post_type='wdslp-wds-log') group by MONTH(post_date),YEAR(post_date) order by post_date desc limit 12) a order by ye asc,mo asc" );
-
-    $postcounts = array();
-    foreach ( $postcountbymonth as $pc ) {
-	$month = $pc->mo;
-	if ( count( $month ) < 1 ) {
-	  $month = '0' . $month;
+    $postquery = array();
+    foreach ( $query->posts as $post ) {
+	$date = explode( ' ', $post->post_date );
+	if ( !isset( $postquery[ $date[ 0 ] ] ) ) {
+	  $postquery[ $date[ 0 ] ] = 0;
 	}
-	$postcounts[] = array( 'date' => "new Date('$pc->ye-$month-01T24:00:00Z')", 'value' => $pc->co );
+	$postquery[ $date[ 0 ] ] += 1;
+
+	if ( $list ) {
+	  $id_task = get_post_meta( $post->ID, DT_TEXTDOMAIN . '_id', true );
+	  if ( !isset( $postid[ $id_task ] ) ) {
+	    $postid[ $id_task ] = 0;
+	  }
+	  $postid[ $id_task ] += 1;
+	}
+    }
+
+    foreach ( $postquery as $key => $count ) {
+	$postcounts[] = array( 'date' => "new Date('" . $key . "T24:00:00Z')", 'value' => $count );
     }
     echo '<div id="postactivity"></div>'
     . '<script>' .
     'MG.data_graphic({
-        title: "' . __( 'Show the task activity in the last 12 months', DT_TEXTDOMAIN ) . '",
+        title: "' . sprintf( __( 'The %s tasks activity of last 12 months', DT_TEXTDOMAIN ), count( $query->posts ) ) . '",
         data: ' . str_replace( ')"', ')', str_replace( '"new', 'new', wp_json_encode( $postcounts, JSON_NUMERIC_CHECK ) ) ) . ',
         width: 800,
         height: 400,
@@ -97,35 +137,66 @@ class DT_Graphs {
         }
     });'
     . '</script>';
+    if ( $list ) {
+	echo '<ul>';
+	foreach ( $postid as $key => $count ) {
+	  echo '<li><a href="' . get_permalink( $key ) . '" target="_blank">' . get_the_title( $key ) . '</a> ' . $count . ' ' . __( 'times', DT_TEXTDOMAIN ) . '</li>';
+	}
+	echo '</ul>';
+    }
   }
 
   /**
    * Generate a graph of the task daily activity
    *
    * @since    1.0.0
+   * @param	   array $atts The attribute.
    */
-  public function task_daily_activity() {
-    global $wpdb;
+  public function task_daily_activity( $atts ) {
+    extract( shortcode_atts( array(
+	  'list' => true,
+			  ), $atts ) );
+    $args = array(
+	  'post_type' => 'wdslp-wds-log',
+	  'posts_per_page' => -1,
+	  'tax_query' => array(
+		array(
+		    'taxonomy' => 'wds_log_type',
+		    'field' => 'slug',
+		    'terms' => array( 'datask' ),
+		),
+	  ),
+	  'date_query' => array(
+		array(
+		    'after' => '1 month ago',
+		),
+	  ),
+    );
+    $query = new WP_Query( $args );
 
-    $postcountbymonth = $wpdb->get_results( 'select * from (select MONTH(post_date) as mo,DAY(post_date) as day,count(ID) as co from ' . $wpdb->posts . " where (post_status='publish' and post_type='wdslp-wds-log') group by DAY(post_date),MONTH(post_date) order by post_date desc limit 12) a order by day asc,mo asc" );
-
-    $postcounts = array();
-    foreach ( $postcountbymonth as $pc ) {
-	$month = $pc->mo;
-	if ( count( $month ) < 1 ) {
-	  $month = '0' . $month;
+    $postquery = $postid = array();
+    foreach ( $query->posts as $post ) {
+	$date = explode( ' ', $post->post_date );
+	if ( !isset( $postquery[ $date[ 0 ] ] ) ) {
+	  $postquery[ $date[ 0 ] ] = 0;
 	}
-	$day = $pc->day;
-	if ( count( $day ) < 1 ) {
-	  $day = '0' . $day;
-	}
+	$postquery[ $date[ 0 ] ] += 1;
 
-	$postcounts[] = array( 'date' => "new Date('" . date( 'Y' ) . '-' . $month . '-' . $day . "T24:00:00Z')", 'value' => $pc->co );
+	if ( $list ) {
+	  $id_task = get_post_meta( $post->ID, DT_TEXTDOMAIN . '_id', true );
+	  if ( !isset( $postid[ $id_task ] ) ) {
+	    $postid[ $id_task ] = 0;
+	  }
+	  $postid[ $id_task ] += 1;
+	}
     }
-    echo '<div id="postdailyactivity"></div>'
-    . '<script>' .
+
+    foreach ( $postquery as $key => $count ) {
+	$postcounts[] = array( 'date' => "new Date('" . $key . "T24:00:00Z')", 'value' => $count );
+    }
+    echo '<div id="postdailyactivity"></div><script>' .
     'MG.data_graphic({
-        title: "' . __( 'Show the task daily activity in the this months', DT_TEXTDOMAIN ) . '",
+        title: "' . sprintf( __( 'The %s tasks daily activity of this months', DT_TEXTDOMAIN ), count( $query->posts ) ) . '",
         data: ' . str_replace( ')"', ')', str_replace( '"new', 'new', wp_json_encode( $postcounts, JSON_NUMERIC_CHECK ) ) ) . ',
         width: 800,
         height: 400,
@@ -140,6 +211,13 @@ class DT_Graphs {
         }
     });'
     . '</script>';
+    if ( $list ) {
+	echo '<ul>';
+	foreach ( $postid as $key => $count ) {
+	  echo '<li><a href="' . get_permalink( $key ) . '" target="_blank">' . get_the_title( $key ) . '</a> ' . $count . ' ' . __( 'times', DT_TEXTDOMAIN ) . '</li>';
+	}
+	echo '</ul>';
+    }
   }
 
 }
